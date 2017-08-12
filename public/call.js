03659329b328ee786833e4866a8a5393a3dc63e5
Yura Shkuratov <jura913@ya.ru>
2017-08-12 16:05:05 +0300

    add simple dialog

--- public/call.js
+++ public/call.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+
+  function showDialog(dialogData) {
+    var content = "<span class='question-text'>"+dialogData.text+"</span><br>";
+    content += dialogData.buttons.map(function(button) {
+      return "<button class='btn btn-primary answer'>"+button.Text+"</button><br>";
+    }).join('\n');
+    $(".content").html(content);
+  }
+
   console.log('dom ready');
   $('#call').click(function() {
-
-    $.get('http://192.168.1.150/API/test2', function(data) {
-      console.log('data:', data);
+    console.log('ask for phone');
+    // var testData = { text: '+7324', buttons: [{text: 'Ответил', id: '1'}, {text: 'Не ответил', id: '2'}] }
+    // showDialog(testData);
+    $.get('http://192.168.1.150/API/calls.init', function(data) {
+      showDialog(data.result);
     })
 
   })

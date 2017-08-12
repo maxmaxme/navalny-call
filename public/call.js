11934a739cfc09125727e399c81d2e6196a330a2
Yura Shkuratov <jura913@ya.ru>
2017-08-12 16:26:35 +0300

    dialog system work via REST!

--- public/call.js
+++ public/call.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-
+  var host = 'http://192.168.1.150';
   function showDialog(dialogData) {
     var content = "<span class='question-text'>"+dialogData.text+"</span><br>";
     content += dialogData.buttons.map(function(button) {
-      return "<button class='btn btn-primary answer'>"+button.Text+"</button><br>";
+      return "<button class='btn btn-primary answer' id='btn"+button.ID+"'>"+button.Text+"</button><br>";
     }).join('\n');
     $(".content").html(content);
+  }
+
+  function enableButtons(dialogData) {
+    dialogData.buttons.forEach(function(button) {
+      $('#btn'+button.ID).on('click', function() {
+        loadNewDialog(button.ID);
+      })
+    })
+  }
+
+  function loadNewDialog(id) {
+    console.log('load dialog', id);
+    $.get(host+'/API/calls.button?buttonID='+id, function(data) {
+      createDialog(data.result);
+    })
+  }
+
+  function createDialog(dialogData) {
+    console.log('create dialog');
+    showDialog(dialogData);
+    enableButtons(dialogData);
   }
 
   console.log('dom ready');
@@ -13,8 +34,8 @@ $(document).ready(function() {
     console.log('ask for phone');
     // var testData = { text: '+7324', buttons: [{text: 'Ответил', id: '1'}, {text: 'Не ответил', id: '2'}] }
     // showDialog(testData);
-    $.get('http://192.168.1.150/API/calls.init', function(data) {
-      showDialog(data.result);
+    $.get(host+'/API/calls.init', function(data) {
+      createDialog(data.result);
     })
 
   })
